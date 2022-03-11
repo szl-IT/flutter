fdc69196d26548527ebb4579ce8f6e17c0e9bccf
songzhiling <17630035658@163.com>
2022-03-11 18:02:59 +0800

    commit:动画效果添加

--- lib/customview/customview_demo.dart
+++ lib/customview/customview_demo.dart
@@ -1,19 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_study/customview/view/my_view.dart';
 
-class CustomViewPage extends StatelessWidget {
+class CustomViewPage extends StatefulWidget {
   const CustomViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyViewState();
+  }
+}
+
+///左右抖动动画
+class MyViewState extends State<CustomViewPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> animation;
+
+  late int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 300), vsync: this);
+
+    animation = Tween(begin: -10.0, end: 10.0).animate(_animationController);
+
+    _animationController.forward();
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController.reset();
+        _animationController.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("自定义View"),
-      ),
-      body: CustomPaint(
-        painter: MyView(),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("自定义View"),
+        ),
+        body: LayoutBuilder(builder: (context, constraints) {
+          return AnimatedBuilder(
+              animation: animation,
+              builder: (context, _) {
+                return Transform(
+                  transform: Matrix4.rotationZ(animation.value * pi / 180),
+                  alignment: Alignment.center,
+                  child: Center(
+                      child: CustomPaint(
+                    painter: MyView(),
+                  )),
+                );
+              });
+        }));
   }
 }
--- lib/customview/view/my_view.dart
+++ lib/customview/view/my_view.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,12 +10,38 @@ class MyView extends CustomPainter {
   MyView() {
     _paint = Paint()
       ..color = Colors.orange
+      ..strokeWidth = 0.0
+      ..style = PaintingStyle.stroke
       ..isAntiAlias = true;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(const Offset(20, 20), 20, _paint);
+    _drawCircle(canvas, size);
+  }
+
+  ///绘制圆
+  void _drawCircle(Canvas canvas, Size size) {
+    // canvas.drawCircle(const Offset(100, 100), 40, _paint);
+    // canvas.drawLine(const Offset(100, 0), const Offset(100, 200), _paint);
+
+    _paint.color = Colors.blueAccent;
+    _paint.strokeWidth = 5;
+    var rect = Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2), radius: 100);
+    canvas.drawArc(rect, 0, pi, true, _paint);
+
+    _paint.style = PaintingStyle.fill;
+    _paint.color = Colors.red;
+    canvas.drawCircle(Offset(size.width / 2, size.height + 30), 20, _paint);
+
+    _paint.color = Colors.orange;
+    canvas.drawCircle(
+        Offset(size.width / 2 + 20, size.height + 70), 20, _paint);
+
+    _paint.color = Colors.purple;
+    canvas.drawCircle(
+        Offset(size.width / 2 + 60, size.height + 50), 20, _paint);
   }
 
   @override
